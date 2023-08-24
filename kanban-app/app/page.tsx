525da109f37e9bff51df0124537d38f2e2ebce92
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-08-25 01:51:26 +0300

    feat: base layout / page files will be updated

--- kanban-app/app/page.tsx
+++ kanban-app/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <main>Hello world!</main>
+import { getCookie } from "./cookies.server"
+import MainBoard from "@layouts/MainBoard"
+import Navbar from "@components/Navbar"
+import MainLayout from "@layouts/MainLayout"
+
+export default async function Home() {
+    const viewType = await getCookie("viewType")
+
+    return (
+        <MainLayout viewTypeCookie={viewType}>
+            <Navbar />
+            <MainBoard />
+        </MainLayout>
+    )
 }
