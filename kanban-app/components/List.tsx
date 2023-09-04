fe4cb1558930f68b9fe0be484a1b377a66458261
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-09-04 20:04:08 +0300

    feat: List component changes

--- kanban-app/components/List.tsx
+++ kanban-app/components/List.tsx
@@ -1,11 +1,39 @@
+import { useDroppable } from "@dnd-kit/core"
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable"
 import { cn } from "@/utilities/cn"
+import { SortableCard } from "./Card"
+import type { Task } from "@constants/tasks"
 
 export interface ListProps extends React.HTMLAttributes<HTMLDivElement> {
-    children: React.ReactNode
+    // children: React.ReactNode
+    tasks: Task[]
+    id: string
 }
 
-const List = ({ children, className }: ListProps) => {
-    return <div className={cn("bg-purple-500", className)}>{children}</div>
+const List = ({ className, id, tasks, ...props }: ListProps) => {
+    // console.log("children", children, id)
+    // console.log("sortableContext - id:", id)
+    const { setNodeRef } = useDroppable({
+        id,
+    })
+
+    return (
+        <SortableContext
+            id={id}
+            items={tasks}
+            strategy={verticalListSortingStrategy}
+        >
+            <div
+                className="flex flex-col gap-4 bg-yellow-500 p-4"
+                ref={setNodeRef}
+                {...props}
+            >
+                {tasks.map((task) => (
+                    <SortableCard task={task} key={task.id} />
+                ))}
+            </div>
+        </SortableContext>
+    )
 }
 
 export default List
