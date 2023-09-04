94a7c8a92d5654e97776840c2d5bd771df0010ab
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-09-04 20:03:12 +0300

    fix: constants fix

--- kanban-app/constants/tasks.tsx
+++ kanban-app/constants/tasks.tsx
@@ -1,8 +1,8 @@
-export type PriorityType = 'LOW' | 'MEDIUM' | 'HIGH'
-export type ListType = 'backlog' | 'todo' | 'done'
+export type PriorityType = "LOW" | "MEDIUM" | "HIGH"
+export type ListType = "backlog" | "todo" | "done"
 
 export type Task = {
-    id: number
+    id: string
     tag: string
     title: string
     priority: PriorityType
@@ -10,63 +10,62 @@ export type Task = {
     assignedTo: string
     listType: ListType | Omit<ListType, string>
 }
-  
 
 const defaultTasks: Task[] = [
-  {
-    id: 1,
-    tag: 'TSK',
-    title: 'Being able to rename and edit users lorem rename and edit users',
-    priority: 'HIGH',
-    weight: 8,
-    assignedTo: 'eda',
-    listType: "backlog"
-  },
-  {
-    id: 1,
-    tag: 'EPC',
-    title: 'Being able to rename and edit users lorem rename and edit users',
-    priority: 'LOW',
-    weight: 5,
-    assignedTo: 'eda',
-    listType: 'todo',
-  },
-  {
-    id: 2,
-    tag: 'EPC',
-    title: 'Being able to rename and edit users lorem rename and edit users',
-    priority: 'LOW',
-    weight: 5,
-    assignedTo: 'onur',
-    listType: 'todo',
-  },
-  {
-    id: 2,
-    tag: 'TSK',
-    title: 'Being able to rename and edit users lorem rename and edit users',
-    priority: 'MEDIUM',
-    weight: 3,
-    assignedTo: 'tolga',
-    listType: 'backlog',
-  },
-  {
-    id: 3,
-    tag: 'TSK',
-    title: 'Being able to rename and edit users lorem rename and edit users',
-    priority: 'HIGH',
-    weight: 8,
-    assignedTo: 'onur',
-    listType: 'backlog',
-  },
-  {
-    id: 4,
-    tag: 'TSK',
-    title: 'Being able to rename and edit users lorem rename and edit users',
-    priority: 'LOW',
-    weight: 13,
-    assignedTo: 'onur',
-    listType: 'done',
-  },
+    {
+        id: "1",
+        tag: "TSK",
+        title: "Being able to rename and edit users lorem rename and edit users",
+        priority: "HIGH",
+        weight: 8,
+        assignedTo: "eda",
+        listType: "backlog",
+    },
+    {
+        id: "2",
+        tag: "EPC",
+        title: "Being able to rename and edit users lorem rename and edit users",
+        priority: "LOW",
+        weight: 5,
+        assignedTo: "eda",
+        listType: "todo",
+    },
+    {
+        id: "3",
+        tag: "EPC",
+        title: "Being able to rename and edit users lorem rename and edit users",
+        priority: "LOW",
+        weight: 5,
+        assignedTo: "onur",
+        listType: "todo",
+    },
+    {
+        id: "4",
+        tag: "TSK",
+        title: "Being able to rename and edit users lorem rename and edit users",
+        priority: "MEDIUM",
+        weight: 3,
+        assignedTo: "tolga",
+        listType: "backlog",
+    },
+    {
+        id: "5",
+        tag: "TSK",
+        title: "Being able to rename and edit users lorem rename and edit users",
+        priority: "HIGH",
+        weight: 8,
+        assignedTo: "onur",
+        listType: "backlog",
+    },
+    {
+        id: "6",
+        tag: "TSK",
+        title: "Being able to rename and edit users lorem rename and edit users",
+        priority: "LOW",
+        weight: 13,
+        assignedTo: "onur",
+        listType: "done",
+    },
 ]
 
 export default defaultTasks
