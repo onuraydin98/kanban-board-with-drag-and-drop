d240bd4d2d238a89e2aaf945547d0193d75b4430
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-09-04 20:03:26 +0300

    fix: cookies.server type fix

--- kanban-app/app/cookies.server.ts
+++ kanban-app/app/cookies.server.ts
@@ -1,12 +1,12 @@
 import { cookies } from "next/headers"
 
-export async function getCookie(key: string): Promise<string | undefined> {
+export async function getCookie(key: string): Promise<string | null> {
     const cookiesObj = cookies()
 
     if (cookiesObj.has(key)) {
-        const cookieValue = cookiesObj.get(key)?.value
+        const cookieValue = cookiesObj.get(key)!.value
         return cookieValue
     }
 
-    return undefined
+    return null
 }
