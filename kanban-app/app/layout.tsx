00cd68e0a1fa86775cec09450ae3fe045d758782
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-09-23 02:20:42 +0300

    refactor: font-family will be changed

--- kanban-app/app/layout.tsx
+++ kanban-app/app/layout.tsx
@@ -1,10 +1,12 @@
 import { cn } from "@/utilities/cn"
 import "./globals.css"
 import { ThemeProvider } from "@components/ThemeProvider"
-import { Inter } from "next/font/google"
+import { Inter, Roboto_Serif } from "next/font/google"
 import type { Metadata } from "next"
 
+// Will be decided
 const inter = Inter({ subsets: ["latin"] })
+const robotoSerif = Roboto_Serif({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
     title: "Kanban board template",
@@ -18,7 +20,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={cn(inter.className, "h-screen")}>
+            <body className={cn(robotoSerif.className, "h-screen")}>
                 {" "}
                 <ThemeProvider
                     attribute="class"
