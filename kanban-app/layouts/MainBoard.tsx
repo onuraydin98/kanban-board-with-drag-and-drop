ac49d9d6e0e1f65a3faeac8354f5ad893a99e910
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-09-23 02:22:36 +0300

    feat: layouts will be changed

--- kanban-app/layouts/MainBoard.tsx
+++ kanban-app/layouts/MainBoard.tsx
@@ -1,24 +1,193 @@
 "use client"
 
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ViewContext } from "./MainLayout"
+import {
+    DndContext,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+    closestCorners,
+    closestCenter,
+    DragOverlay,
+    type DragStartEvent,
+    type DragEndEvent,
+    type DragOverEvent,
+} from "@dnd-kit/core"
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable"
 import List from "@components/List"
+import { Card } from "@components/Card"
 import { cn } from "@utilities/cn"
+import {
+    defaultLists,
+    defaultTasks,
+    ListType,
+    type Task,
+} from "@constants/index"
+import useListStore from "@stores/useListStore"
+import generateId from "@/utilities/generateId"
 
 export default function MainBoard() {
     const viewType = useContext(ViewContext)
 
+    const [storeLists, storeBulkUpdate, storeChangeTaskListType] = useListStore(
+        state => [state.lists, state.bulkUpdate, state.changeTaskListType],
+    )
+
+    const storeListv2 = useListStore.getState().lists
+
+    const [lists, setLists] = useState(defaultLists)
+    const [tasks, setTasks] = useState<Task[]>(defaultTasks)
+
+    const [activeTask, setActiveTask] = useState<Task | null>(null)
+
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        }),
+    )
+
+    function handleDragStart(event: DragStartEvent) {
+        const { active } = event
+        const { id } = active
+
+        setActiveTask(active.data.current?.task)
+
+        // console.log("drag start")
+        console.log("active task", active.data.current?.task)
+        const activeIndex = storeLists[
+            active.data.current?.task.listType as ListType
+        ].findIndex(t => t.id === id)
+        console.log("drag start active index", activeIndex)
+        // console.log("tasks", tasks)
+        // console.log(tasks.findIndex(t => t.id === id))
+    }
+
+    function handleDragEnd(event: DragEndEvent) {
+        setActiveTask(null)
+
+        const { active, over } = event
+        if (!over) return
+
+        const activeId = active.id
+        const overId = over.id
+
+        if (activeId === overId) return
+
+        // console.log("event", event)
+        // console.log("activeId", activeId)
+        // console.log("overId", overId)
+    }
+
+    function handleDragOver(event: DragOverEvent) {
+        const { active, over } = event
+        if (!over) return
+
+        const activeId = active.id
+        const overId = over.id
+
+        if (activeId === overId) return
+
+        // console.log("active", active, "over", over)
+        // console.log(
+        //     "active-index",
+        //     tasks.findIndex(t => t.id === activeId),
+        //     "over-index",
+        //     tasks.findIndex(t => t.id === overId),
+        // )
+
+        const activeTaskListType = active.data.current?.task
+            .listType as ListType
+
+        const isTaskOverTask =
+            (active.data.current?.type && over.data.current?.type) === "Task"
+
+        const isTaskOverList =
+            active.data.current?.type === "Task" &&
+            over.data.current?.type === "List"
+
+        if (isTaskOverTask) {
+            const overTaskListType = over.data.current?.task
+                .listType as ListType
+
+            const activeIndex = storeLists[activeTaskListType].findIndex(
+                t => t.id === activeId,
+            )
+            const overIndex = storeLists[overTaskListType].findIndex(
+                t => t.id === overId,
+            )
+
+            // Tasks' list types are not same
+            if (activeTaskListType !== overTaskListType) {
+                storeChangeTaskListType(
+                    activeId as string,
+                    activeTaskListType,
+                    overTaskListType,
+                    true,
+                )
+            }
+
+            // Same list type tasks
+            if (activeTaskListType === overTaskListType) {
+                storeBulkUpdate(
+                    arrayMove(
+                        storeLists[activeTaskListType],
+                        activeIndex,
+                        overIndex,
+                    ),
+                    activeTaskListType,
+                )
+            }
+        }
+
+        // Task over list
+        if (isTaskOverList) {
+            const overTaskListType = over.data.current?.data.id
+
+            storeChangeTaskListType(
+                activeId as string,
+                activeTaskListType,
+                overTaskListType,
+            )
+        }
+    }
+
     return (
-        <main
-            className={cn(
-                "grid gap-8 p-4 transition-all duration-300 ease-in-out",
-                viewType === "grid" && "md:flex-1 md:grid-flow-col",
-                viewType === "list" && "grid-cols-1",
-            )}
+        <DndContext
+            sensors={sensors}
+            collisionDetection={closestCorners}
+            onDragStart={handleDragStart}
+            // onDragEnd={handleDragEnd}
+            onDragOver={handleDragOver}
         >
-            <List>This is card 1</List>
-            <List>This is card 2</List>
-            <List>This is card 3</List>
-        </main>
+            <main
+                className={cn(
+                    "grid grid-cols-1 gap-8 p-4 transition-all duration-300 ease-in-out",
+                    viewType === "grid" &&
+                        "md:grid md:flex-1 md:grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))]",
+                    // viewType === "list" && "grid-cols-1",
+                )}
+            >
+                {Object.keys(storeLists).map(listType => (
+                    <List
+                        key={listType}
+                        tasks={storeLists[listType as ListType]}
+                        id={listType}
+                    />
+                ))}
+                <DragOverlay>
+                    {activeTask?.id ? (
+                        <Card
+                            task={activeTask!}
+                            id={activeTask.id ?? "overlay-card"}
+                        >
+                            This is card {activeTask.id}
+                        </Card>
+                    ) : null}
+                </DragOverlay>
+            </main>
+        </DndContext>
     )
 }
--- kanban-app/layouts/MainLayout.tsx
+++ kanban-app/layouts/MainLayout.tsx
@@ -1,86 +1,92 @@
 "use client"
 
-import { Mail, Menu, LayoutGrid, List, Kanban } from "lucide-react"
+// TODO: Bu layout'u client'ten kurtarabilir miyiz sonra bak!
+
 import {
-    useState,
-    type PropsWithChildren,
-    type ChangeEvent,
-    type SetStateAction,
-    type Dispatch,
-    createContext,
-} from "react"
-import debounce from "@/utilities/debounce"
-import { ModeToggle } from "@components/ToggleTheme"
-import { ButtonWithIcon } from "@components/ui/button"
-import { Input } from "@components/ui/input"
+    Mail,
+    Kanban,
+    Home,
+    MessageSquare,
+    ListChecks,
+    Compass,
+    RefreshCw,
+    Plus,
+    HelpCircle,
+    BellRing,
+    type LucideProps,
+} from "lucide-react"
+import { useState, createContext, type PropsWithChildren } from "react"
+import { Button, ButtonWithIcon } from "@components/ui/button"
+import Navbar from "@components/Navbar"
+import { cn } from "@utilities/cn"
+
+import Avatar from "@components/Avatar"
 import { Separator } from "@components/ui/separator"
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@components/ui/sheet"
-import { setCookie } from "cookies-next"
 
 export const ViewContext = createContext<ViewType>("grid")
 
 export type ViewType = "grid" | "list" | null
 
-const ViewToggle = ({
-    opts,
-}: {
-    opts: [ViewType, Dispatch<SetStateAction<ViewType>>]
-}) => {
-    const [viewState, setState] = [...opts]
-    const viewType = viewState === "grid" ? "list" : "grid"
-
-    return (
-        <ButtonWithIcon
-            icon={
-                viewState === "grid" ? (
-                    <List className="h-4 w-4" />
-                ) : (
-                    <LayoutGrid className="h-4 w-4" />
-                )
-            }
-            onClick={() => {
-                setCookie("viewType", viewType, {
-                    maxAge: 7900000,
-                })
-                setState((prev) => {
-                    if (prev === "grid") return "list"
-                    return "grid"
-                })
-            }}
-            variant="outline"
-            className="max-md:hidden"
-        />
-    )
+type NavigationMenu = {
+    text: string
+    href: string
+    icon: React.ReactElement<LucideProps> // WTF - LucideIcon
+    isActive: boolean
 }
 
-const SidebarSheet = ({
-    button,
-}: {
-    button: React.ReactElement<HTMLButtonElement>
-}) => {
-    return (
-        <Sheet>
-            <SheetTrigger asChild>{button}</SheetTrigger>
-            <SheetContent side={"left"}>
-                <SheetHeader>
-                    <SheetTitle>Are you sure absolutely sure?</SheetTitle>
-                    <SheetDescription>
-                        This action cannot be undone. This will permanently
-                        delete your account and remove your data from our
-                        servers.
-                    </SheetDescription>
-                </SheetHeader>
-            </SheetContent>
-        </Sheet>
-    )
-}
+const upperNavigationMenu: NavigationMenu[] = [
+    {
+        text: "Dashboard",
+        href: "#dashboard",
+        icon: <Home className="h-5 w-5" />,
+        isActive: true,
+    },
+    {
+        text: "Feedback",
+        href: "#feedback",
+        icon: <MessageSquare className="h-5 w-5" />,
+        isActive: false,
+    },
+    {
+        text: "Task",
+        href: "#task",
+        icon: <ListChecks className="h-5 w-5" />,
+        isActive: false,
+    },
+    {
+        text: "Roadmap",
+        href: "#roadmap",
+        icon: <Compass className="h-5 w-5" />,
+        isActive: false,
+    },
+    {
+        text: "Changelog",
+        href: "#changelog",
+        icon: <RefreshCw className="h-5 w-5" />,
+        isActive: false,
+    },
+]
+
+const lowerNavigationMenu: NavigationMenu[] = [
+    {
+        text: "Invite People",
+        href: "#invite",
+        icon: <Plus className="h-5 w-5" />,
+        isActive: false,
+    },
+    {
+        text: "Help & Comm.",
+        href: "#help-community",
+        icon: <HelpCircle className="h-5 w-5" />,
+        isActive: false,
+    },
+    {
+        text: "Notifications",
+        href: "#notifications",
+        icon: <BellRing className="h-5 w-5" />,
+        isActive: false,
+    },
+]
 
 const MainLayout = ({
     viewTypeCookie,
@@ -90,60 +96,74 @@ const MainLayout = ({
         (viewTypeCookie as ViewType) ?? "grid",
     )
 
-    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.value === "" || e.target.value.length < 2) return
-
-        console.log("input value", e.target.value) // search alg
-    }
-
     return (
         <div className="flex h-full">
-            <aside className="h-full w-48 p-3 max-lg:hidden xl:w-60 2xl:w-80">
+            <aside className="fixed flex h-full w-52 flex-col p-3 max-lg:hidden 2xl:w-64 ">
                 <div className="flex h-[3.25rem] items-start justify-center gap-1 ">
                     <Kanban className="h-6 w-6" />
                     <h2 className="[line-height:1em]">Board</h2>
                 </div>
-                <nav className="flex flex-col">
-                    {[...new Array(5)].map((_, idx) => (
-                        <ButtonWithIcon
-                            icon={<Mail className="h-4 w-4" />}
-                            variant="ghost"
-                            key={idx}
-                            className="text-sm sm:text-base 2xl:text-xl"
-                        >
-                            {`Navigation ${idx}`}
-                        </ButtonWithIcon>
-                    ))}
-                </nav>
-            </aside>
-            <div className="flex flex-1 flex-col">
-                <div className="flex h-16 w-full justify-between bg-purple-500 px-6 py-3">
-                    <div className="flex items-center gap-4">
-                        <SidebarSheet
-                            button={
+                {/* Navigation Menus */}
+                <nav className="flex flex-1 flex-col items-start justify-between">
+                    <ul className="flex w-full flex-col gap-2">
+                        {upperNavigationMenu.map((element, idx) => (
+                            <li key={`up-nav-${idx}`}>
                                 <ButtonWithIcon
-                                    icon={<Menu className="h-4 w-4" />}
+                                    icon={element.icon}
                                     variant="ghost"
-                                    className="px-3 py-0 lg:hidden"
-                                />
-                            }
+                                    key={element.text}
+                                    type="button"
+                                    className={cn(
+                                        "w-full items-center justify-start gap-2 text-sm sm:text-base 2xl:text-xl",
+                                        element.isActive && "bg-slate-700",
+                                    )}
+                                >
+                                    <a href={element.href}>{element.text}</a>
+                                </ButtonWithIcon>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <ul className="flex w-full flex-col gap-2">
+                        {lowerNavigationMenu.map((element, idx) => (
+                            <li key={`down-nav-${idx}`}>
+                                <ButtonWithIcon
+                                    icon={element.icon}
+                                    variant="ghost"
+                                    key={element.text}
+                                    type="button"
+                                    className={cn(
+                                        "w-full items-center justify-start gap-2 text-sm sm:text-base 2xl:text-xl",
+                                        element.isActive && "bg-slate-700",
+                                    )}
+                                >
+                                    <a href={element.href}>{element.text}</a>
+                                </ButtonWithIcon>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <Separator
+                    orientation="horizontal"
+                    className="sm my-2 h-[2px] bg-white"
+                />
+                <div className="w-full">
+                    <Button
+                        variant="ghost"
+                        type="button"
+                        className="w-full items-center justify-start gap-2 bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-sm text-transparent sm:text-base 2xl:text-xl"
+                    >
+                        <Avatar
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
                         />
-                        <Separator
-                            orientation="vertical"
-                            className="lg:hidden"
-                        />
-                        <div className="max-lg:pl-4">
-                            <Input
-                                placeholder="Search"
-                                className="w-40 text-base transition-[width] duration-300 sm:w-48 sm:focus-visible:w-60 "
-                                onChange={debounce(handleSearch, 500)}
-                            />
-                        </div>
-                    </div>
-                    <div className="flex items-center gap-4">
-                        <ViewToggle opts={[viewType, setViewType]} />
-                        <ModeToggle />
-                    </div>
+                        Eric Clarkin
+                    </Button>
+                </div>
+            </aside>
+            <div className="ml-52 flex flex-1 flex-col max-lg:ml-0 2xl:ml-64 ">
+                <div className="flex h-16 w-full justify-between bg-purple-500 px-6 py-3">
+                    <Navbar viewTypeConfig={[viewType, setViewType]} />
                 </div>
                 <div className="flex flex-1 flex-col">
                     <ViewContext.Provider value={viewType}>
