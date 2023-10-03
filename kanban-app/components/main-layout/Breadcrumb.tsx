956eb858128492c3bcd3105e84fb81b9ad53dd3a
onuraydin98 <32241352+onuraydin98@users.noreply.github.com>
2023-10-03 04:19:25 +0300

    feat: breadcrumb dummy links will be added

--- kanban-app/components/main-layout/Breadcrumb.tsx
+++ kanban-app/components/main-layout/Breadcrumb.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@utilities/cn"
+import { MoveLeft } from "lucide-react"
 
 const links = [
     {
@@ -19,12 +20,23 @@ const links = [
 
 const Breadcrumb = () => {
     return (
-        <nav className={cn("flex gap-4 divide-x-2 divide-gray-800 px-6 py-3")}>
+        <nav
+            className={cn(
+                "flex items-center divide-x-2 divide-gray-800 px-6 py-3 transition-[transform,colors]",
+            )}
+        >
+            <a
+                href="/"
+                className="pr-3 duration-200 ease-linear hover:-translate-x-1 dark:hover:text-slate-300"
+            >
+                <MoveLeft className="h-5 w-5" />
+            </a>
+
             {links.map(link => (
                 <a
-                    key={link.slug}
+                    key={link.title}
                     href={link.slug}
-                    className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent"
+                    className="px-3 duration-200 ease-linear dark:hover:text-slate-300"
                 >
                     {link.title}
                 </a>
